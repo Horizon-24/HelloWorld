c434b28c9e80426d75d36b0942f568d40fd65c51
DELL <DELL@DESKTOP-IQSN7RA>
2021-11-20 02:02:58 +0800

    添加项目文件。

---
+++ HelloWorld/Function.cpp
@@ -0,0 +1,11 @@
+#include "Function.h"
+/// <summary>
+/// 两数相加
+/// </summary>
+/// <param name="a">第一个数</param>
+/// <param name="b">第二个数</param>
+/// <returns>和</returns>
+int Sum(int a, int b)
+{
+	return a + b;
+}
---
+++ HelloWorld/Main.cpp
@@ -0,0 +1,11 @@
+#include "Framework.h"
+#include "Function.h"
+
+using namespace std;
+
+int main(void)
+{
+	Sum(5, 6);
+	cout << "Hello World" << endl;
+	return 0;
+}
